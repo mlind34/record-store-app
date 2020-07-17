2a6f72053b573f6e82d44913aef25ef4bfed9265
Maxwell Lind <lindmax@flip2.engr.oregonstate.edu>
2020-07-17 14:40:55 -0700

    max commit 7/17 #4

--- static/index.js
+++ static/index.js
@@ -1,41 +1,41 @@
-const customerTable = document.getElementById('customerTable');
+// const customerTable = document.getElementById('customerTable');
 
 
-customerTable.addEventListener('click', function(){
-    let target = event.target;
-    if(target.value == 'Update'){
-        var confirm = makeConfirm(target)
-        updateRow(target.id);
-    }
-});
+// customerTable.addEventListener('click', function(){
+//     let target = event.target;
+//     if(target.value == 'Update'){
+//         var confirm = makeConfirm(target)
+//         updateRow(target.id);
+//     }
+// });
 
 
-const updateRow = (id) => {
-    var rowToUpdate = document.getElementById(id);
+// const updateRow = (id) => {
+//     var rowToUpdate = document.getElementById(id);
     
-    dataCell = rowToUpdate.getElementsByClassName('table-input');
+//     dataCell = rowToUpdate.getElementsByClassName('table-input');
 
-    for(var i = 0; i < dataCell.length; i++){
-        input = document.createElement('input');
+//     for(var i = 0; i < dataCell.length; i++){
+//         input = document.createElement('input');
 
-        input.setAttribute('type', 'text');
+//         input.setAttribute('type', 'text');
 
-        input.setAttribute('size', '17')
+//         input.setAttribute('size', '17')
 
-        input.setAttribute('value', dataCell[i].innerHTML)
+//         input.setAttribute('value', dataCell[i].innerHTML)
 
-        dataCell[i].innerHTML = '';
+//         dataCell[i].innerHTML = '';
 
-        dataCell[i].append(input);
-    }
-}
+//         dataCell[i].append(input);
+//     }
+// }
 
 
-const makeConfirm = (button) => {
-    button.setAttribute('value', 'Confirm');
-    button.style.backgroundColor = 'rgb(100, 150, 240)'
-    return button;
-}
+// const makeConfirm = (button) => {
+//     button.setAttribute('value', 'Confirm');
+//     button.style.backgroundColor = 'rgb(100, 150, 240)'
+//     return button;
+// }
 
 
 // fetch('http://flip2.engr.oregonstate.edu:4521/customers')
