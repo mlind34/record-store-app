c4c471b7bfa45722968eef62a4e8d6c6479c33d2
Maxwell Lind <lindmax@flip2.engr.oregonstate.edu>
2020-07-17 16:07:11 -0700

    max commit 7/17 # 5

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, jsonify, make_response
 app = Flask(__name__)
 
 @app.route('/')
-def test():
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
     return render_template('home.html')
 
 @app.route('/customers', methods=['GET'])
@@ -42,12 +46,12 @@ def show_customer():
         }
     ]
 
-    return render_template('customers.html', customers=customers)
+    return render_template('customers.html', customers=customers, title='Customers')
     
 
 @app.route('/customers/add-customer')
 def add_customer():
-    return render_template('addcustomer.html')
+    return render_template('addcustomer.html', title='Add Customer')
 
 
 @app.route('/distributors')
@@ -81,15 +85,14 @@ def show_distributor():
         'phone': '(888) 703-6517'
         },
     ]
-    return render_template('distributors.html', distributors=distributors)
+    return render_template('distributors.html', distributors=distributors, title='Distributors')
 
 @app.route('/distributors/add-distributor')
 def add_distributor():
 
-    return render_template('addistributor.html')
+    return render_template('addistributor.html', title='Add Distributor')
 
 
-<<<<<<< HEAD
 
 
 @app.route('/records')
@@ -122,15 +125,52 @@ def show_records():
         'name': '(888) 703-6517'
         },
     ]
-    return render_template('records.html', records = records)
+    return render_template('records.html', records = records, title='Records')
 
 @app.route('/records/add-record')
 def add_record():
 
-    return render_template('addrecord.html')
-=======
-@app.route('/records')
-def show_inventory():
+    return render_template('addrecord.html', title='Add Record')
 
-    return render_template('records.html')
->>>>>>> 2a6f72053b573f6e82d44913aef25ef4bfed9265
+@app.route('/purchases')
+def view_purchases():
+    purchases = [
+        {
+            'id': 709,
+            'purchaseDate': '01-23-2019',
+            'paymentMethod': 'Card',
+            'paid': True,
+            'totalPrice': 56.98 
+        },
+        {
+            'id': 710,
+            'purchaseDate': '11-29-2017',
+            'paymentMethod': 'Cash',
+            'paid': True,
+            'totalPrice': 45.18 
+        },
+        {
+            'id': 711,
+            'purchaseDate': '05-03-2018',
+            'paymentMethod': 'Card',
+            'paid': True,
+            'totalPrice': 5.67 
+        }
+    ]
+    return render_template('purchases.html', purchases=purchases, title='Purchases')
+
+
+@app.route('/purchases/add-purchase')
+def add_purchase():
+
+    return render_template('addpurchase.html', title='Add Purchase')
+
+
+@app.route('/orders')
+def view_orders():
+    return render_template('orders.html', title='Orders')
+
+
+@app.route('/orders/add-order')
+def add_order():
+    return render_template('addorder.html', title='Add Order')
