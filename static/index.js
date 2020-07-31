97bfccc99194bd9de209b00d280de2a8860583d6
Maxwell Lind <lindmax@flip1.engr.oregonstate.edu>
2020-07-31 15:58:36 -0700

    final commit 7/31

--- static/index.js
+++ static/index.js
@@ -1,47 +1,81 @@
 let dataTable = document.getElementById('dataTable');
 // console.log(dataTable);
-let buttons = dataTable.querySelectorAll('input');
+// let buttons = dataTable.querySelectorAll('input');
 // console.log(buttons);
 
 // for(let i in buttons) {
 console.log(dataTable);
-let buttons = document.querySelectorAll('input');
-console.log(buttons);
+// let buttons = document.querySelectorAll('input');
+// console.log(buttons);
 recordsAdded = [];
-for(let i in buttons) {
 
-  // if(buttons[i].value == 'Purchases') {
-    //   let data = {
-    //     id: buttons[i].id
-    //   };
-      //contextJson = JSON.stringify(context);
-      // buttons[i].addEventListener("click", (event) => {
-        // let subReq = new XMLHttpRequest();
-        // subReq.open("POST", "http://flip3.engr.oregonstate.edu:5199/custpurchases", true);
-        // subReq.setRequestHeader("content-type", "application/json");
-        // subReq.addEventListener("load", () => {
-        //     document.querySelector('html').innerHTML = subReq.response;
-        // })
-        // data = JSON.stringify(data);
-        // subReq.send(data);
-        // event.preventDefault();
-        // window.location.href = '/custpurchases/'+buttons[i].id
 
-        // fetch('http://flip3.engr.oregonstate.edu:5199/custpurchases', {
-        //     method: 'POST',
-        //     mode: 'cors',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: contextJson,
-        // }).then(function (response) {
-        //     console.log(response);
-        // })
-//       })
+
+
+// for(let i in buttons) {
+
+//     if(buttons[i].value == 'Purchases') {
+//       //   let data = {
+//       //     id: buttons[i].id
+//       //   };
+//         //contextJson = JSON.stringify(context);
+//         buttons[i].addEventListener("click", (event) => {
+//           // let subReq = new XMLHttpRequest();
+//           // subReq.open("POST", "http://flip3.engr.oregonstate.edu:5199/custpurchases", true);
+//           // subReq.setRequestHeader("content-type", "application/json");
+//           // subReq.addEventListener("load", () => {
+//           //     document.querySelector('html').innerHTML = subReq.response;
+//           // })
+//           // data = JSON.stringify(data);
+//           // subReq.send(data);
+//           // event.preventDefault();
+//           window.location.href = '/custpurchases/'+buttons[i].id
+  
+//           // fetch('http://flip3.engr.oregonstate.edu:5199/custpurchases', {
+//           //     method: 'POST',
+//           //     mode: 'cors',
+//           //     headers: {
+//           //         'content-type': 'application/json'
+//           //     },
+//           //     body: contextJson,
+//           // }).then(function (response) {
+//           //     console.log(response);
+//           // })
+//         })
+//     }
+//     if(buttons[i].value == 'Add to Order') {
+//       recordsAdded = [];
+//       buttons[i].addEventListener("click", (event) => {
+//         makeConfirm(buttons[i]);
+//         event.preventDefault;
+//         recordsAdded.push(buttons[i].id);
+//         console.log(recordsAdded);
+//       });
+//     }
+//     if(buttons[i].value == 'Submit Records for Order') {
+//       console.log("Test");
+//       buttons[i].addEventListener("click", (event) => {
+//         data = {
+//           recordIDs: recordsAdded
+//         };
+//         let subReq = new XMLHttpRequest();
+//           subReq.open("POST", "http://flip3.engr.oregonstate.edu:5199/purchases/add-purchase/final", true);
+//           subReq.setRequestHeader("content-type", "application/json");
+//           subReq.addEventListener("load", () => {
+//             // window.location.href = '/purchases/add-purchase/final'
+//           })
+//           data = JSON.stringify(data);
+//           subReq.send(data);
+//           event.preventDefault();
+//       });
+//     }
 //   }
-// }
 
 
+
+
+
+// UPDATE FUNCTIONS
 dataTable.addEventListener('click', (event) =>{
     let target = event.target;
     if(target.value == 'Update'){
@@ -102,83 +136,9 @@ function createInput(cell, name, value) {
     cell.append(input)
 }
 
-      
-  
-  if(buttons[i].value == 'Add to Order') {
-    recordsAdded = [];
-    buttons[i].addEventListener("click", (event) => {
-      makeConfirm(buttons[i]);
-      event.preventDefault;
-      recordsAdded.push(buttons[i].id);
-      console.log(recordsAdded);
-    });
-  }
-  if(buttons[i].value == 'Submit Records for Order') {
-    console.log("Test");
-    data = {
-      recordIDs: recordsAdded
-    };
-    buttons[i].addEventListener("click", (event) => {
-      let subReq = new XMLHttpRequest();
-        subReq.open("POST", "http://flip3.engr.oregonstate.edu:5199/purchases/add-purchase/final", true);
-        subReq.setRequestHeader("content-type", "application/json");
-        subReq.addEventListener("load", () => {
-          // window.location.href = '/purchases/add-purchase/final'
-        })
-        data = JSON.stringify(data);
-        subReq.send(data);
-        event.preventDefault();
-    });
-  }
-}
-
-dataTable.addEventListener('click', (event) =>{
-    let target = event.target;
-    if(target.value == 'Update'){
-        var confirm = makeConfirm(target)
-        updateRow(target.id);
-    }
-});
 
 
-
-// const updateRow = (id) => {
-//     var rowToUpdate = document.getElementById(id);
-    
-//     dataCell = rowToUpdate.getElementsByClassName('table-input');
-
-//     checkCell = rowToUpdate.getElementsByClassName('ui checkbox');
-
-//     for(var i = 0; i < dataCell.length; i++){
-  
-//         input = document.createElement('input');
-
-//         input.setAttribute('type', 'text');
-
-//         input.setAttribute('size', '17');
-        
-//         input.setAttribute('value', dataCell[i].innerText);
-
-//         dataCell[i].innerHTML = '';
-
-//         dataCell[i].append(input);
-       
-//     }
-// }
-
-    // checkInput = document.createElement('input');
-    // input.setAttribute('type', 'checkbox');
-    // console.log(input)
-    // if(checkCell.checked == True){
-    //     input.setAttribute('checked', 'True')
-    // } else {
-    //     input.setAttribute('checked', 'False')
-    // }
-    // checkCell = input
-
-
-
-const makeConfirm = (button) => {
+function makeConfirm(button) {
     if(button.value == 'Update') {
       button.setAttribute('value', 'Confirm');
       button.style.backgroundColor = 'rgb(100, 150, 240)'
