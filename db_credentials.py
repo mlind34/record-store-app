ca2c8e7ba119785b6f0f3375d5bc0f8da4423b6f
Max Lind <maxlind34@outlook.com>
2020-08-25 10:59:24 -0500

    commit - no pycache

--- db_credentials.py
+++
@@ -1,6 +0,0 @@
-# DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-host = 'classmysql.engr.oregonstate.edu'
-user = 'cs340_lindmax'  # CHANGE ME
-passwd = '2191'  # CHANGE ME
-db = 'cs340_lindmax'  # CHANGE ME
-charset = 'utf8'
